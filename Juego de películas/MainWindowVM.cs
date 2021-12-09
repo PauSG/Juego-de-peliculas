118aac3de0c81718745bf25ebbdc62a32e115c8e
PauSG <pau.sanso@iesdoctorbalmis.com>
2021-12-09 14:59:52 +0100

    Avanzamos Casi toda la parte grafica, y empiezo el codebehind

---
+++ Juego de películas/MainWindowVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_de_películas
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+
+        private Pelicula peliculaActual;
+
+        public Pelicula PeliculaActual
+        {
+            get { return peliculaActual; }
+            set
+            {
+                peliculaActual = value;
+                this.NotifyPropertyChanged("PeliculaActual");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
